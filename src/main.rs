bc6806f62b6cec6c33c2a986dbb459d68ddd037c
Philip Rying <philiprying@gmail.com>
2025-10-07 12:32:16 +0200

    wowo

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,37 @@
-use std::env
-use std::fmt
-
+use std::env;
+use std::fs::create_dir;
+use std::path::{Path, PathBuf};
 
 #[link(name = "NotchHelper")] // defaults to dylib
 unsafe extern "C" {
     fn show_status_item();
 }
 
-const TMP_FOLDER_NAME = "TMPDIR";
+const TMP_FOLDER_NAME: &str = "TMPDIR";
+const NOTCH_FOLDER: &str = "notch";
+
+fn create_or_get_dir(tmp_folder: &'static str, notch_folder: &'static str) -> PathBuf {
+    let tmp_folder = env::var(tmp_folder)
+        .unwrap_or_else(|_| panic!("Error no enviroment folder found for {}", tmp_folder));
+
+    let tmp_path = Path::new(&tmp_folder);
+    let notch_path = tmp_path.join(notch_folder);
+
+    println!("{:?}", notch_path);
+    if !notch_path.exists() {
+        create_dir(&notch_path)
+            .unwrap_or_else(|_| panic!("Error could not create folder in {}", tmp_folder));
+    }
+
+    notch_path
+}
 
 fn main() {
-    let TMP_FOLDER = env::var(TMP_FOLDER_NAME).expect(format!("Error: environment folder {} not present. Asserting", TMP_FOLDER_NAME).as_str());
+    let notch_path = create_or_get_dir(TMP_FOLDER_NAME, NOTCH_FOLDER);
 
-    println!("{}", TMP_FOLDER);
-    
-    unsafe { show_status_item(); }
+    unsafe {
+        show_status_item();
+    }
 }
+
+// fn get_files() -> Vec<String> {}
