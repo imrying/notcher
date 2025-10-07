6e02735fa57135e92430ddf6b96032ffb4f62eee
Philip Rying <philiprying@gmail.com>
2025-10-07 11:49:33 +0200

    RUST FUCK U

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
+use std::env
+use std::fmt
+
+
 #[link(name = "NotchHelper")] // defaults to dylib
 unsafe extern "C" {
     fn show_status_item();
 }
 
+const TMP_FOLDER_NAME = "TMPDIR";
+
 fn main() {
+    let TMP_FOLDER = env::var(TMP_FOLDER_NAME).expect(format!("Error: environment folder {} not present. Asserting", TMP_FOLDER_NAME).as_str());
+
+    println!("{}", TMP_FOLDER);
+    
     unsafe { show_status_item(); }
 }
